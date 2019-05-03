c96db057c15916ac83d5eb3e15f139f557c85c9d
SpyrosRoum <spyrosr1@gmail.com>
2019-05-03 15:26:57 +0300

    Refacted everything

--- cogs/Names.py
+++ cogs/Names.py
@@ -6,32 +6,58 @@ class Names(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # def change_region(self, member: discord.Member, new_region):
+    #     regions = ["[EU]", "[SA]", "[AF]", "[NA]", "[NE]", "[AS]", "[OC]"]
+    #     platforms = ["[PC]", "[XBOX]", "[PS4]"]
+    #     old_platforms = []
+    #     old_region = ""
+
+    #     old = member.display_name.split()
+    #     print(f"Before: {old}")
+    #     for i, word in enumerate(old):
+    #         if word in platforms:
+    #             old_platforms.append(old.pop(i))
+    #         elif word in regions:
+    #             old_region = old.pop(i)
+    #     print(f"After: {old}")
+        
+    #     if new_region == old_region:
+    #         new = " ".join(old_platforms)
+    #         for word in old:
+    #             new += f" {word}"
+    #         return new
+        
+    #     new = " ".join(old_platforms)
+    #     new += f" {new_region}"
+    #     for word in old:
+    #         new += f" {word}"
+    #     return new
+
     def change_region(self, member: discord.Member, new_region):
         regions = ["[EU]", "[SA]", "[AF]", "[NA]", "[NE]", "[AS]", "[OC]"]
-        platforms = ["[PC]", "[XBOX]", "[PS4]"]
-        old_platforms = []
-        old_region = ""
+        regions.remove(new_region)
+
+        if new_region in member.display_name:
+            new = member.display_name.replace(new_region, "")
+            return new
+
+        new = new_region
 
         old = member.display_name.split()
-        print(f"Before: {old}")
-        for i, word in enumerate(old):
-            if word in platforms:
-                old_platforms.append(old.pop(i))
-            elif word in regions:
-                old_region = old.pop(i)
-        print(f"After: {old}")
-        
-        if new_region == old_region:
-            new = " ".join(old_platforms)
-            for word in old:
-                new += f" {word}"
-            return new
-        
-        new = " ".join(old_platforms)
-        new += f" {new_region}"
         for word in old:
-            new += f" {word}"
+            if word not in regions: 
+                new += f" {word}"
+
+        
         return new
+        
+
+    def change_plat(self, member: discord.Member, new_plat):
+        if new_plat in member.display_name:
+            new = member.display_name.replace(new_plat, "")
+            return new
+
+        return f"{new_plat} {member.display_name}"
 
     @commands.command(brief='')
     async def EU(self, ctx):
@@ -79,34 +105,20 @@ class Names(commands.Cog):
     async def PC(self, ctx):
         """PC"""
         member = ctx.author
-
-        if member.display_name.startswith("[PC]"):
-            await member.edit(nick=member.display_name.replace("[PC]", ""))
-            return 
-
-        await member.edit(nick=f"[PC] {member.display_name}")
+        await member.edit(nick=self.change_plat(member, "[PC]"))
+        
 
     @commands.command(brief='')
     async def ps4(self, ctx):
         """ps4"""
         member = ctx.author
-
-        if member.display_name.startswith("[PS4]"):
-            await member.edit(nick=member.display_name.replace("[PS4]", ""))
-            return
-
-        await member.edit(nick=f"[PS4] {member.display_name}")
+        await member.edit(nick=self.change_plat(member, "[PS4]"))
 
     @commands.command(brief='')
     async def xbox(self, ctx):
         """xbox"""
         member = ctx.author
-
-        if member.display_name.startswith("[XBOX]"):
-            await member.edit(nick=member.display_name.replace("[XBOX]", ""))
-            return
-
-        await member.edit(nick=f"[XBOX] {member.display_name}")
+        await member.edit(nick=self.change_plat(member, "[XBOX]"))
 
 def setup(bot):
     bot.add_cog(Names(bot))
